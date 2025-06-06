c977a1b71434417a2ea2df493a2e97da8497373a
NxNiki <michaelniki1988@gmail.com>
2025-06-06 11:45:56 -0700

    [fix] create emr submit jobs.

--- infra/bootstrap/install-package.sh
+++ infra/bootstrap/install-package.sh
@@ -25,14 +25,14 @@ if [[ $1 == *.txt ]]; then
 
   filename=$(basename "$1")
   aws s3 cp "$1" "/home/hadoop/$filename"
-  sudo python3 -m pip install -r "/home/hadoop/$filename"
+  sudo python3 -m pip install --upgrade --ignore-installed -r "/home/hadoop/$filename"
 
 elif [[ $1 == *.whl ]]; then
   echo "Installing Python wheel package: $1"
 
   filename=$(basename "$1")
   aws s3 cp "$1" "/home/hadoop/$filename"
-  sudo python3 -m pip install "/home/hadoop/$filename"
+  sudo python3 -m pip install --ignore-installed "/home/hadoop/$filename"
 
 else
   echo "Unknown file format. Please provide a .txt or .whl file."
