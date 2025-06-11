f01f993c0494493e6432c7f0fc0e1504c109dc28
NxNiki <michaelniki1988@gmail.com>
2025-06-11 11:08:48 -0700

    minor emr job improvements

--- infra/deploy_emr.py
+++ infra/deploy_emr.py
@@ -84,7 +84,6 @@ def start_emr_cluster(
         "Ec2SubnetId": "subnet-02571e70cb058d27a",  # your public subnet here
         "EmrManagedMasterSecurityGroup": "sg-069ce0aa8db40f042",
         "EmrManagedSlaveSecurityGroup": "sg-0df70a2677f24ff31",
-        "AutoTerminationPolicy": {"IdleTimeout": 600},  # terminate clusters after it being idle for 600 seconds
     }
 
     job_flow_args = {
@@ -96,6 +95,7 @@ def start_emr_cluster(
         "JobFlowRole": "EMR_EC2_DefaultRole",
         "ServiceRole": "EMR_DefaultRole",
         "VisibleToAllUsers": True,
+        "AutoTerminationPolicy": {"IdleTimeout": 600},  # terminate clusters after it being idle for 600 seconds
     }
 
     if bootstrap_script_uri and package_uri:
