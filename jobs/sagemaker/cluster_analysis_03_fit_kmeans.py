adf4db4ed6b5d5ef34121bb2d47ca6e2f78cce5c
NxNiki <michaelniki1988@gmail.com>
2025-06-17 17:38:17 -0700

    [feat] add fit_kmeans step to cluster analysis

--- jobs/sagemaker/cluster_analysis_03_fit_kmeans.py
+++ jobs/sagemaker/cluster_analysis_03_fit_kmeans.py
@@ -26,38 +26,40 @@ def scale_features(df_features: pd.DataFrame, df_scale: pd.DataFrame) -> pd.Data
     return df_scaled
 
 
-output_path = "."
+if __name__ == "__main__":
 
-wucaishen_files = list_s3_files(S3_BUCKET, "wucaishen_processed_data", r"wucaishen_grouped_stat_output_25.*\.csv$")
-wucaishen_data = read_files(
-    wucaishen_files, local_cache_path=f"{output_path}/output/wucaishen_grouped_stat_output_25.csv", reload=False
-)
+    output_path = "."
 
-non_features = ["group_id", "loginname", "start_time"]
-important_features = json.load(open(f"{output_path}/features/important_features.json", "r"))
-important_features = important_features[:25]
-print(important_features)
-
-features_log = json.load(open(f"{output_path}/features/log_transform_features.json", "r"))
-features_log = [f for f in features_log if f in important_features]
-print(features_log)
-
-wucaishen_data = log_transform(wucaishen_data, features_log)
-
-scale_params = pd.read_csv(f"{output_path}/features/standardized_features_top_25_parameters.csv", index_col=0)
-
-wucaishen_data = scale_features(wucaishen_data, scale_params.T)
-
-kmeans = load(f"{output_path}/models/kmeans_model.pkl")
-cluster_labels = kmeans.predict(wucaishen_data[important_features])
-
-# 4. Attach cluster labels to new data
-wucaishen_data["cluster"] = cluster_labels
-
-print(wucaishen_data.head(5))
-
-for cluster in np.unique(cluster_labels):
-    print(f"save cluster: {cluster}")
-    wucaishen_data.loc[wucaishen_data["cluster"] == cluster, :].drop(columns="cluster").to_csv(
-        f"{output_path}/output/grouped_data_2025_cluster_{cluster}.csv", index=False
+    wucaishen_files = list_s3_files(S3_BUCKET, "wucaishen_processed_data", r"wucaishen_grouped_stat_output_25.*\.csv$")
+    wucaishen_data = read_files(
+        wucaishen_files, local_cache_path=f"{output_path}/output/wucaishen_grouped_stat_output_25.csv", reload=False
     )
+
+    non_features = ["group_id", "loginname", "start_time"]
+    important_features = json.load(open(f"{output_path}/features/important_features.json", "r"))
+    important_features = important_features[:25]
+    print(important_features)
+
+    features_log = json.load(open(f"{output_path}/features/log_transform_features.json", "r"))
+    features_log = [f for f in features_log if f in important_features]
+    print(features_log)
+
+    wucaishen_data = log_transform(wucaishen_data, features_log)
+
+    scale_params = pd.read_csv(f"{output_path}/features/standardized_features_top_25_parameters.csv", index_col=0)
+
+    wucaishen_data = scale_features(wucaishen_data, scale_params.T)
+
+    kmeans = load(f"{output_path}/models/kmeans_model.pkl")
+    cluster_labels = kmeans.predict(wucaishen_data[important_features])
+
+    # 4. Attach cluster labels to new data
+    wucaishen_data["cluster"] = cluster_labels
+
+    print(wucaishen_data.head(5))
+
+    for cluster in np.unique(cluster_labels):
+        print(f"save cluster: {cluster}")
+        wucaishen_data.loc[wucaishen_data["cluster"] == cluster, :].drop(columns="cluster").to_csv(
+            f"{output_path}/output/grouped_data_2025_cluster_{cluster}.csv", index=False
+        )
