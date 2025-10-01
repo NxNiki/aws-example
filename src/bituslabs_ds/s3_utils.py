dba6ea0cef2ceb4655a061bb70450c4eaaf94508
NxNiki <michaelniki1988@gmail.com>
2025-10-01 14:55:59 -0700

    change sagemaker to user_segmentation and add mahjiang_streak jobs

--- src/bituslabs_ds/s3_utils.py
+++ src/bituslabs_ds/s3_utils.py
@@ -61,10 +61,26 @@ def parse_s3_path(s3_path: str) -> Tuple[str, str]:
 
 
 def read_to_pandas_df(bucket: str, key: str, columns: Optional[List[str]] = None) -> pd.DataFrame:
-    """Read a CSV file from S3 and return it as a Pandas DataFrame."""
+    """
+    Read a CSV or Parquet file from S3 and return it as a Pandas DataFrame.
+
+    :param bucket: S3 bucket name
+    :param key: S3 object key
+    :param columns: Optional list of columns to read
+    :return: Pandas DataFrame
+    """
     bucket = parse_bucket_name(bucket)
-    response = s3_client.get_object(Bucket=bucket, Key=key)
-    return pd.read_csv(response["Body"], usecols=columns)
+    _, ext = os.path.splitext(key.lower())
+    if ext == ".csv":
+        response = s3_client.get_object(Bucket=bucket, Key=key)
+        return pd.read_csv(response["Body"], usecols=columns)
+    elif ext == ".parquet":
+        s3 = fs.S3FileSystem(region=REGION)
+        s3_path = f"{bucket}/{key}"
+        with s3.open_input_file(s3_path) as f:
+            return pd.read_parquet(f, columns=columns)
+    else:
+        raise ValueError(f"Unsupported file extension for S3 object: {key}")
 
 
 def write_df_to_s3(data: Union[pd.DataFrame, SparkDataFrame], bucket: str, key: str) -> None:
