ff7ff3f6a0de1e59434cf05e1b4cddaeaf3df2f3
NxNiki <michaelniki1988@gmail.com>
2026-04-10 11:34:56 -0700

    [FIX] bug in ss01 and ss02 etl query: filter default group for default mathtables

--- jobs/ss01_wucaishen/etl_game_stats_daily_by_user_group.py
+++ jobs/ss01_wucaishen/etl_game_stats_daily_by_user_group.py
@@ -100,6 +100,7 @@ def generate_query(stats_agg_col: AggCol, start_date: str):
                 END AS ai_group
             FROM
                 user_bets AS t
+            WHERE t.ab_group_id != '{AI_GROUP_ID}'
 
             UNION ALL
 
