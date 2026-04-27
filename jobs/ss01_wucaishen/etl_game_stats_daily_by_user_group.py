efa870943a52edd6ed38e0f0f57ad0722e0174a6
NxNiki <michaelniki1988@gmail.com>
2026-04-27 11:21:18 -0700

    [fix] replace script_id with math_table_id for ss01

--- jobs/ss01_wucaishen/etl_game_stats_daily_by_user_group.py
+++ jobs/ss01_wucaishen/etl_game_stats_daily_by_user_group.py
@@ -54,7 +54,7 @@ def generate_query(stats_agg_col: AggCol, start_date: str):
         SELECT
             t.user_id,
             t.created_at,
-            t.script_id AS mathtable,
+            t.math_table_id AS mathtable,
             t.bet_amount,
             t.actual_payout AS payout,
             t.bet_type,
@@ -69,8 +69,8 @@ def generate_query(stats_agg_col: AggCol, start_date: str):
             LAG(t.bet_amount) OVER (PARTITION BY t.user_id ORDER BY t.spin_id, t.created_at) AS prev_bet_amount,
             COUNT(t.user_id) OVER (PARTITION BY t.user_id) AS user_bet_count,
             CASE
-                WHEN LAG(t.script_id) OVER (PARTITION BY t.user_id ORDER BY t.spin_id, t.created_at) IS NULL THEN 0
-                WHEN LAG(t.script_id) OVER (PARTITION BY t.user_id ORDER BY t.spin_id, t.created_at) <> t.script_id THEN 1
+                WHEN LAG(t.math_table_id) OVER (PARTITION BY t.user_id ORDER BY t.spin_id, t.created_at) IS NULL THEN 0
+                WHEN LAG(t.math_table_id) OVER (PARTITION BY t.user_id ORDER BY t.spin_id, t.created_at) <> t.math_table_id THEN 1
                 ELSE 0
             END AS mathtable_change
         FROM
